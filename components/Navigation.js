8ae02b68fd5589818dd12445ea4fdda7332a0f80
abu-git <abu.sammie@gmail.com>
2022-04-03 23:37:10 +0200

    close icon added to navigation for mobile devices

--- components/Navigation.js
+++ components/Navigation.js
@@ -5,13 +5,18 @@ import { useState } from 'react'
 function Navigation() {
     //state is used to toggle navbar on mobile devices
     const [displayMobileMenu, setDisplayMobileMenu] = useState(false)
+
+    //state for close icon
+    const [displayCloseIcon, setDisplayCloseIcon] = useState(false)
     
     const displayMenu = (e) => {
         e.preventDefault()
         if(displayMobileMenu === false){
             setDisplayMobileMenu(true)
+            setDisplayCloseIcon(true)
         }else{
             setDisplayMobileMenu(false)
+            setDisplayCloseIcon(false)
         }
     }
 
@@ -28,11 +33,20 @@ function Navigation() {
                 <li className='text-lg cursor-pointer'>Contact</li>
             </ul>
             {/* menu button ---hidden unless on small devices */}
+            {displayMobileMenu === false &&
             <button className="md:hidden" onClick={displayMenu}>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                     <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" />
                 </svg>
-            </button>
+            </button>}
+            {/* -----------close icon display config---------- */}
+            {displayCloseIcon === true &&
+                <button onClick={displayMenu}>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
+            }
         </nav>
 
         {/*---- conditional rendering for mobile menu display ----*/}
@@ -44,7 +58,6 @@ function Navigation() {
                 <li className='py-1'>Contact</li>
             </ul>
         }
-        
     </div>
   )
 }
