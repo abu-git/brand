1a2f65095637b0a2b9aaeadf388958ea7980e7cc
abu-git <abu.sammie@gmail.com>
2022-04-04 00:51:49 +0200

    about page progress

--- components/Navigation.js
+++ components/Navigation.js
@@ -32,7 +32,7 @@ function Navigation() {
                 <Link href="/about"><li className='text-lg cursor-pointer'>About</li></Link>
                 <li className='text-lg cursor-pointer'>Contact</li>
             </ul>
-            {/* menu button ---hidden unless on small devices */}
+            {/* menu button ---hidden unless on small devices ---boolean used for close icon config */}
             {displayMobileMenu === false &&
             <button className="md:hidden" onClick={displayMenu}>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
@@ -42,8 +42,8 @@ function Navigation() {
             {/* -----------close icon display config---------- */}
             {displayCloseIcon === true &&
                 <button onClick={displayMenu}>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
             }
@@ -52,7 +52,7 @@ function Navigation() {
         {/*---- conditional rendering for mobile menu display ----*/}
         {displayMobileMenu === true && 
             <ul className='md:hidden text-right font-semibold py-5'>
-                <li className='py-1'>Home</li>
+                <Link href="/"><li className='py-1'>Home</li></Link>
                 <li className='py-1'>Articles</li>
                 <li className='py-1'>About</li>
                 <li className='py-1'>Contact</li>
--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import Navigation from '../components/Navigation'
+import Footer from '../components/Footer'
 
 function About() {
   return (
+    <>
         <Navigation />
+        <section className='mt-8 px-6 grid grid-cols-1 md:grid-cols-2'>
+            <div className='md:text-5xl text-4xl font-bold text-right pr-5 pb-5'>
+                <h1>We are a group of African creatives in different places in the world with a common goal to create and give back to the creative community. This is one of our contributions.</h1>
+            </div>
+            <div className='bg-white rounded drop-shadow-lg px-3 py-5 items-center'>
+                <h4 className='text-2xl font-light'><span className='text-3xl font-semibold'>Brand.</span> is a media company that aims to highlight the works of young, hard-working and deserving creatives from the continent. We aim to cover a large spectrum of music genres and art. We want to connect the diasporan with the nerdy African child.</h4>
+                <br />
+
+                <h4 className='text-2xl font-light'>Hopefully, we can give voice to the unheard and inspire the young one. Stay with us on this journey.</h4>
+            </div>
+        </section>
+        <Footer />
+    </>
     
   )
 }
