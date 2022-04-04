0ad39e9071d9042d00b44dd0a9d8261e3a592a65
abu-git <abu.sammie@gmail.com>
2022-04-04 23:07:43 +0200

    web application layout progress

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -10,7 +10,7 @@ function Posts() {
         <section className='mt-8 px-6 lg:px-48 flex flex-col'>
             <h2 className='mb-5 p-2 md:text-xl text-md font-semibold'>All Posts</h2>
             {/* post item */}
-            <div className='flex bg-white rounded overflow-hidden drop-shadow-lg hover:drop-shadow-xl mb-4 max-h-96'>
+            <div className='flex bg-white rounded overflow-hidden drop-shadow-lg hover:drop-shadow-xl mb-4 max-h-96 cursor-pointer'>
                 <div className='w-1/3 brightness-75'> 
                     <Image
                         width={20}
@@ -21,11 +21,11 @@ function Posts() {
                 </div>
                 
                 <div className='w-2/3 flex items-center'>
-                    <h3 className='w-fit text-xs md:text-2xl lg:text-3xl xl:text-4xl font-light p-3 items-center'><span className='text-md font-semibold'>Timi Kei delivers a stellar oratory performance on The Purpose EP.</span> Production credits going to iamMHP also known as MHP.</h3>
+                    <h3 className='w-fit text-xs md:text-2xl lg:text-3xl font-light p-3 items-center'><span className='text-md font-semibold'>Timi Kei delivers a stellar oratory performance on The Purpose EP.</span> Production credits going to iamMHP also known as MHP.</h3>
                 </div>
             </div>
 
-            <div className='flex bg-white rounded overflow-hidden drop-shadow-lg hover:drop-shadow-xl mb-4 max-h-96'>
+            <div className='flex bg-white rounded overflow-hidden drop-shadow-lg hover:drop-shadow-xl mb-4 max-h-96 cursor-pointer'>
                 <div className='w-1/3 brightness-75'> 
                     <Image
                         width={20}
@@ -36,11 +36,11 @@ function Posts() {
                 </div>
                 
                 <div className='w-2/3 flex items-center'>
-                    <h3 className='w-fit text-xs md:text-2xl lg:text-3xl xl:text-4xl font-light p-3 items-center'><span className='text-md font-semibold'>'Peace God' EP by Daboomsha and Stoute Louis</span> We dive deep into this masterpiece from two Nigerian creatives: Daboomsha and Stoute Louis</h3>
+                    <h3 className='w-fit text-xs md:text-2xl lg:text-3xl font-light p-3 items-center'><span className='text-md font-semibold'>'Peace God' EP by Daboomsha and Stoute Louis.</span> We dive deep into this masterpiece from two Nigerian creatives: Daboomsha and Stoute Louis</h3>
                 </div>
             </div>
 
-            <div className='flex bg-white rounded overflow-hidden drop-shadow-lg hover:drop-shadow-xl mb-4 max-h-96'>
+            <div className='flex bg-white rounded overflow-hidden drop-shadow-lg hover:drop-shadow-xl mb-4 max-h-96 cursor-pointer'>
                 <div className='w-1/3 brightness-75'> 
                     <Image
                         width={20}
@@ -51,11 +51,11 @@ function Posts() {
                 </div>
                 
                 <div className='w-2/3 flex items-center'>
-                    <h3 className='w-fit text-xs md:text-2xl lg:text-3xl xl:text-4xl font-light p-3 items-center'><span className='text-md font-semibold'>'A Travel Guide for the Broken' by Synik</span> We set our ears on a Zimbabwean creative by the name of Synik. A conceptually pleasing sound offering.</h3>
+                    <h3 className='w-fit text-xs md:text-2xl lg:text-3xl font-light p-3 items-center'><span className='text-md font-semibold'>'A Travel Guide for the Broken' by Synik.</span> We set our ears on a Zimbabwean creative by the name of Synik. A conceptually pleasing sound offering.</h3>
                 </div>
             </div>
 
-            <div className='flex bg-white rounded overflow-hidden drop-shadow-lg hover:drop-shadow-xl mb-4 max-h-96'>
+            <div className='flex bg-white rounded overflow-hidden drop-shadow-lg hover:drop-shadow-xl mb-4 max-h-96 cursor-pointer'>
                 <div className='w-1/3 brightness-75'> 
                     <Image
                         width={20}
@@ -66,7 +66,7 @@ function Posts() {
                 </div>
                 
                 <div className='w-2/3 flex items-center'>
-                    <h3 className='w-fit text-xs md:text-2xl lg:text-3xl xl:text-4xl font-light p-3 items-center'><span className='text-md font-semibold'>'Africore' by Holstar</span> Much is to be said of the Holstar. A Zambian creative, music executive and African music champion. Africore is neatly executed.</h3>
+                    <h3 className='w-fit text-xs md:text-2xl lg:text-3xl font-light p-3 items-center'><span className='text-md font-semibold'>'Africore' by Holstar.</span> Much is to be said of the Holstar. A Zambian creative, music executive and African music champion. Africore is neatly executed.</h3>
                 </div>
             </div>
         </section>
