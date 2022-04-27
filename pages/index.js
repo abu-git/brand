3f47eb2c6af566418a78170aca38bb83edeabb10
abu-git <abu.sammie@gmail.com>
2022-04-28 01:10:53 +0200

    key usage in meta much better

--- pages/index.js
+++ pages/index.js
@@ -31,16 +31,16 @@ export default function Home({posts}) {
       {/* Social media meta tags */}
       <meta property="og:url" content="https://www.tunesketch.com" />
       <meta property="og:type" content="website" />
-      <meta property="og:title" content='We are a group of African creatives in different places in the world with a common goal to create and give back to the creative community' key='title' />
-      <meta property="og:description" content="tunesketch is a media company that aims to highlight the works of young, hard-working and deserving creatives from the continent and worldwide. We aim to cover a large spectrum of music genres and art." />
-      <meta property="og:image" content="/meta.jpg" />
+      <meta property="og:title" content='We are a group of African creatives in different places in the world with a common goal to create and give back to the creative community' key='ogtitle' />
+      <meta property="og:description" content="tunesketch is a media company that aims to highlight the works of young, hard-working and deserving creatives from the continent and worldwide. We aim to cover a large spectrum of music genres and art." key="ogdesc" />
+      <meta property="og:image" content="/meta.jpg" key="ogimg" />
 
       <meta property="twitter:card" content="summary_large_image"/>
       <meta property="twitter:domain" content="tunesketch.com"/>
       <meta property="twitter:url" content="https://www.tunesketch.com/"/>
-      <meta name="twitter:title" content="We are a group of African creatives in different places in the world." key="title"/>
-      <meta name="twitter:description" content="tunesketch is a media company that aims to highlight the works of young, hard-working and deserving creatives."  />
-      <meta name="twitter:image" content='meta.jpg'/>
+      <meta name="twitter:title" content="We are a group of African creatives in different places in the world." key="ttitle"/>
+      <meta name="twitter:description" content="tunesketch is a media company that aims to highlight the works of young, hard-working and deserving creatives." key='tdesc'  />
+      <meta name="twitter:image" content='meta.jpg' key="timg" />
     </Head>
     <motion.div 
       exit={{ opacity: 0 }}
