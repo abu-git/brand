a6c6211186754aed48974c2b0d8b3aec60110fa2
abu-git <abu.sammie@gmail.com>
2022-04-28 00:24:37 +0200

    added new favicon to index.js

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,9 @@ export default function Home({posts}) {
     <Head>
       <title>tunesketch | Creative Showcase</title>
       <meta name="description" content="A small media company established in 2022" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
       {/* Social media meta tags */}
       <meta property="og:url" content="https://tunesketch.com" />
       <meta property="og:type" content="article" />
