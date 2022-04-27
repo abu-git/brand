7d97021b19b161d0c4aface83834d6af61655142
abu-git <abu.sammie@gmail.com>
2022-04-28 01:04:17 +0200

    more meta tags added

--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,8 @@ export default function Home({posts}) {
     <>
     <Head>
       <title>tunesketch | Creative Showcase</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="description" content="A small media company established in 2022" />
       <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
