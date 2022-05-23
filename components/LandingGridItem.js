36d7187bca33916dcc1c108e203dbb2cf11658eb
abu-git <abu.sammie@gmail.com>
2022-05-23 18:01:12 +0200

    date published added to [slug]

--- components/LandingGridItem.js
+++ components/LandingGridItem.js
@@ -7,35 +7,24 @@ function LandingGridItem({post}) {
     
     return (
         <Link href={`/posts/${post.attributes.slug}`} passHref>
-            <div className='flex flex-col cursor-pointer bg-white rounded drop-shadow-lg overflow-hidden md:rounded md:drop-shadow-md                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        hover:drop-shadow-2xl dark:bg-slate-800 dark:text-gray-100'>
-                    <div className='relative'>
-                        <Image 
-                            width={433}
-                            height={433}
-                            alt={post.attributes.title}
-                            src={`/${post.attributes.gridImage}`}
-                            layout='responsive'
-                            className='brightness-50'
-                        />
-                        <h3 className='absolute brightness-100 font-bold text-3xl -mt-36 text-white px-5'>{post.attributes.title}</h3>
-                    </div>
-                    <span className='text-md lg:text-lg text-gray-700 dark:text-gray-100 mt-2 mb-2 text-justify px-3'>
-                        {post.attributes.description}
-                    </span>
-                    <span className='flex justify-end mb-3'>
-                        <h5 className='px-3 text-xs text-gray-500 dark:text-gray-100 self-center'>{(new Date(post.attributes.publishedAt)).toDateString()}</h5>
-                        {/*<div className='px-3 content-center'>
-                            <Image 
-                                width={20}
-                                height={20}
-                                alt="author"
-                                src="/sam-modified.png"
-                            />
-                        </div>*/}
-                    </span>
+            <div className='flex flex-col cursor-pointer bg-white rounded drop-shadow-lg overflow-hidden md:rounded md:drop-shadow-md hover:drop-shadow-2xl dark:bg-slate-800 dark:text-gray-100'>
+                <div className='relative'>
+                    <Image 
+                        width={433}
+                        height={433}
+                        alt={post.attributes.title}
+                        src={`/${post.attributes.gridImage}`}
+                        layout='responsive'
+                        className='brightness-50'
+                    />
+                    <h3 className='absolute brightness-100 font-bold text-3xl -mt-36 text-white px-5'>{post.attributes.title}</h3>
+                </div>
+                <span className='text-md lg:text-lg text-gray-700 dark:text-gray-100 mt-2 mb-2 text-justify px-3'>
+                    {post.attributes.description}
+                </span>
             </div>
         </Link>
     )
 }
 
-export default LandingGridItem                                                                                                                                                                                                                                                                                                                                 
+export default LandingGridItem
