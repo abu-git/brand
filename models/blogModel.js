55199f84a22e6f39179452d5c7d26b9354a91f42
Samuel Abu <abu.sammie@gmail.com>
2022-10-26 13:53:00 +0200

    blog schema started

--- models/blogModel.js
+++ models/blogModel.js
@@ -1,1 +1,18 @@
 import { Schema, model, models } from 'mongoose'
+
+
+const blogSchema = new Schema({
+    title: String,
+    description: String,
+    content: String,
+    gridImage: String,
+    slug: String,
+    spotify: String,
+    youtube: String,
+    itunes: String,
+    metaImage: String
+})
+
+const Blog = models.Blog || model('Blog', blogSchema)
+
+export default Blog
