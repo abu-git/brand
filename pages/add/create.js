5e0c0d7692c24f5a8cbe96ac3bbc41663469ade2
Samuel Abu <abu.sammie@gmail.com>
2022-10-28 16:27:32 +0200

    mongodb mongoose progress

--- pages/add/create.js
+++ pages/add/create.js
@@ -1,7 +1,36 @@
 import React from 'react'
+import connectMongo from '../../utils/connectMongoDB'
+import Blog from '../../models/blogModel'
 
 function Create() {
-  return (
+
+    const createBlog = async () => {
+        const res = await fetch('/api/blog/add', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+
+            body: JSON.stringify({
+                title: req.body.title,
+                description: req.body.description,
+                content: req.body.content,
+                more_content: req.body.content,
+                gridImage: req.body.gridImage,
+                slug: req.body.slug,
+                spotify: req.body.spotify,
+                youtube: req.body.youtube,
+                itunes: req.body.itunes,
+                metaImage: req.body.metaImage
+            })
+        })
+
+        const data = await res.json()
+        console.log(data)
+    }
+    
+
+    return (
     <div className='bg-slate-100 py-7 px-24'>
         <h2 className='text-lg font-semibold text-center'>Create Blog Post</h2>
         <form className='bg-white mx-5 my-6 py-4 px-3'>
---
+++ pages/api/blog/add.js
@@ -0,0 +1,27 @@
+import connectMongo from "../../../utils/connectMongoDB"
+import Blog from "../../../models/blogModel"
+
+/**
+ * 
+ * @param {import('next').NextApiRequest} req 
+ * @param {import('next').NextApiResponse} res 
+ */
+
+export default async function addBlog(req, res){
+
+    try{
+        const { title, description, content, more_content, gridImage, slug, spotify, youtube, itunes, metaImage } = req.body
+        console.log('Connecting to MongoDB...')
+        await connectMongo()
+        console.log('Connected to MongoDB...')
+
+        console.log('Creating Document')
+        const blog = await Blog.create(req.body)
+        console.log('Created Document')
+
+        res.json({blog})
+    }catch(error){
+        console.log(error)
+        res.json({error})
+    }
+}
