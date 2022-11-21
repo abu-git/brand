9eac1690b633ac29cefd5865b6917f756c1ef8e8
Samuel Abu <abu.sammie@gmail.com>
2022-11-21 12:10:48 +0200

    loader setTimeout reduced 1000

--- pages/_app.js
+++ pages/_app.js
@@ -24,7 +24,7 @@ function Loading(){
 
   useEffect(() => {
     const handleStart = (url) => (url !== router.asPath) && setLoading(true)
-    const handleComplete = (url) => (url === router.asPath) && setTimeout(() => {setLoading(false)}, 2000)
+    const handleComplete = (url) => (url === router.asPath) && setTimeout(() => {setLoading(false)}, 1000)
 
     router.events.on('routeChangeStart', handleStart)
     router.events.on('routeChangeComplete', handleComplete)
