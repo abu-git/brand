1119dd61077f9e287c636d9d369c11b187713cc4
Samuel Abu <abu.sammie@gmail.com>
2022-12-26 18:01:36 +0200

    odumodublvck odiegwu added

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home({posts, ogImageUrl }) {
   const [gridPosts, setGridPosts] = useState([])
 
   useEffect(() => {
-    setGridPosts(posts.slice(27, 33))
+    setGridPosts(posts.slice(28, 34))
   }, [posts])
 
   return (
@@ -56,7 +56,7 @@ export default function Home({posts, ogImageUrl }) {
           {/*<Navigation />*/}
           <Sidebar />
           {/*<LandingHeader post={posts.data[2].attributes} />{/* -----------> Header post is chosen directly using its id */}
-          <LandingHeader post={posts[33]} />
+          <LandingHeader post={posts[34]} />
           <LandingGrid posts={gridPosts} />
           <Playlists />
           <Footer />
