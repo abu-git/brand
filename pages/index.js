14c88aebfd3f62bfefea9833f4a3f3179ab2a3a1
Samuel Abu <abu.sammie@gmail.com>
2023-01-31 17:39:34 +0200

    grid posts to include big hash

--- pages/index.js
+++ pages/index.js
@@ -64,7 +64,7 @@ export default function Home({posts, ogImageUrl }) {
           {/*<LandingHeader post={posts.data[2].attributes} />{/* -----------> Header post is chosen directly using its id */}
           <AudioPlayer />
           
-          <LandingHeader post={posts[21]} />
+          <LandingHeader post={posts[22]} />
           <LandingGrid posts={gridPosts} />
           <Playlists />
           <Footer />
