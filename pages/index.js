2f066a25e8e02fd62169fe85a2eb997210f10456
Samuel Abu <abu.sammie@gmail.com>
2023-04-24 13:13:03 +0200

    index card list update

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home({posts, ogImageUrl }) {
   const [gridPosts, setGridPosts] = useState([])
 
   useEffect(() => {
-    setGridPosts(posts.slice(35, 41))
+    setGridPosts(posts.slice(36, 42))
   }, [posts])
 
   return (
