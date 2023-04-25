a0288fb187aa3ebfbcdbb2833784b274ac7d948c
Samuel Abu <abu.sammie@gmail.com>
2023-04-25 12:37:43 +0200

    sotd vokulz-journeys end

--- components/AudioPlayer.js
+++ components/AudioPlayer.js
@@ -74,10 +74,10 @@ const AudioPlayer = () => {
 
   return (
     <main className='flex flex-col mt-16 pt-4 pb-4 mx-4 dark:text-gray-100'>
-        <h3 className='text-md md:text-lg font-semibold text-center pb-2'>Song of the Day - <span className="font-bold text-amber-600">Find Me In The Music</span> by Jedi Knight, Garth Soul</h3>
+        <h3 className='text-md md:text-lg font-semibold text-center pb-2'>Song of the Day - <span className="font-bold text-amber-600">Journey&apos;s End</span> by Vokulz</h3>
         <div className='flex justify-center ml-6 md:ml-48 lg:ml-72'>
             <div className={styles.audioPlayer}>
-                <audio ref={audioPlayer} src="/audio/sotd-jedi.mp3" type="audio/mpeg" preload="auto"></audio>
+                <audio ref={audioPlayer} src="/audio/sotd-vokulz.mp3" type="audio/mpeg" preload="auto"></audio>
                 {/*<button className={styles.forwardBackward} onClick={backThirty}><BsArrowLeftShort /> 30</button>*/}
                 <button onClick={togglePlayPause} className={styles.playPause}>
                     {isPlaying ? <FaPause /> : <FaPlay className={styles.play} />}
--- pages/index.js
+++ pages/index.js
@@ -64,7 +64,7 @@ export default function Home({posts, ogImageUrl }) {
           {/*<LandingHeader post={posts.data[2].attributes} />{/* -----------> Header post is chosen directly using its id */}
           <AudioPlayer />
           
-          <LandingHeader post={posts[14]} />
+          <LandingHeader post={posts[23]} />
           <LandingGrid posts={gridPosts} />
           <Playlists />
           <Footer />
