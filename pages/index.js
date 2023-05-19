0dda6af6175999a42e97e5b5739cefcc42f03108
Samuel Abu <abu.sammie@gmail.com>
2023-05-19 11:54:43 +0200

    enji temporarily out of service

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home({posts, ogImageUrl }) {
   const [gridPosts, setGridPosts] = useState([])
 
   useEffect(() => {
-    setGridPosts(posts.slice(38, 44))
+    setGridPosts(posts.slice(39, 45))
   }, [posts])
 
   return (
