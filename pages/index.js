fd0249f0003da25c7863f4b0f98688405faec5ba
Samuel Abu <abu.sammie@gmail.com>
2023-06-16 15:14:25 +0200

    shooterkhumz added

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home({posts, ogImageUrl }) {
   const [gridPosts, setGridPosts] = useState([])
 
   useEffect(() => {
-    setGridPosts(posts.slice(46, 52))
+    setGridPosts(posts.slice(48, 54))
   }, [posts])
 
   return (
