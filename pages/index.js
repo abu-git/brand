3fe2bd457c9761959f747899e239250df80a9c96
Samuel Abu <abu.sammie@gmail.com>
2023-06-23 19:36:57 +0200

    yugen blakrok added

--- pages/index.js
+++ pages/index.js
@@ -64,7 +64,7 @@ export default function Home({posts, ogImageUrl }) {
           {/*<LandingHeader post={posts.data[2].attributes} />{/* -----------> Header post is chosen directly using its id */}
           <AudioPlayer />
           
-          <LandingHeader post={posts[49]} />
+          <LandingHeader post={posts[17]} />
           <LandingGrid posts={gridPosts} />
           <Playlists />
           <Footer />
