25ab78099638ffd0e65e825357e1b5fb19eff8f6
Samuel Abu <abu.sammie@gmail.com>
2023-06-30 14:42:23 +0200

    second sun mind is gold

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home({posts, ogImageUrl }) {
   const [gridPosts, setGridPosts] = useState([])
 
   useEffect(() => {
-    setGridPosts(posts.slice(50, 56))
+    setGridPosts(posts.slice(51, 57))
   }, [posts])
 
   return (
