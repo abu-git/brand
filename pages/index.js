496fe137b04f9240334f6ab06a276fb6883464f1
Samuel Abu <abu.sammie@gmail.com>
2023-07-04 19:57:17 +0200

    index card adjust

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home({posts, ogImageUrl }) {
   const [gridPosts, setGridPosts] = useState([])
 
   useEffect(() => {
-    setGridPosts(posts.slice(52, 58))
+    setGridPosts(posts.slice(53, 59))
   }, [posts])
 
   return (
