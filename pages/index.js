a658c787a7edba78bd076d17fffb1ba6e3b24291
Samuel Abu <abu.sammie@gmail.com>
2023-07-12 16:05:20 +0200

    big hash added

--- pages/index.js
+++ pages/index.js
@@ -64,7 +64,7 @@ export default function Home({posts, ogImageUrl }) {
           {/*<LandingHeader post={posts.data[2].attributes} />{/* -----------> Header post is chosen directly using its id */}
           <AudioPlayer />
           
-          <LandingHeader post={posts[50]} />
+          <LandingHeader post={posts[30]} />
           <LandingGrid posts={gridPosts} />
           <Playlists />
           <Footer />
