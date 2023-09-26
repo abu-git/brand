48bf8bbb99511033a41d85cd957baf9a08be3e7a
Samuel Abu <abu.sammie@gmail.com>
2023-09-26 19:47:56 +0200

    tay iwar summer breeze added

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home({posts, ogImageUrl }) {
   const [gridPosts, setGridPosts] = useState([])
 
   useEffect(() => {
-    setGridPosts(posts.slice(55, 61))
+    setGridPosts(posts.slice(56, 62))
   }, [posts])
 
   return (
