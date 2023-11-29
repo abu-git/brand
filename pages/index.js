6ad7a0a4e12af94cce58fc43615295a8851bdd83
Samuel Abu <abu.sammie@gmail.com>
2023-11-29 14:23:00 +0200

    sotd - seun kuti black thought

--- pages/index.js
+++ pages/index.js
@@ -64,7 +64,7 @@ export default function Home({posts, ogImageUrl }) {
           {/*<LandingHeader post={posts.data[2].attributes} />{/* -----------> Header post is chosen directly using its id */}
           <AudioPlayer />
           
-          <LandingHeader post={posts[62]} />
+          <LandingHeader post={posts[34]} />
           <LandingGrid posts={gridPosts} />
           <Playlists />
           <Footer />
