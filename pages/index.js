defd3b9ec472643389f1958dd414141d13aa888f
Samuel Abu <abu.sammie@gmail.com>
2023-11-30 16:33:27 +0200

    rapsom jump added

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home({posts, ogImageUrl }) {
   const [gridPosts, setGridPosts] = useState([])
 
   useEffect(() => {
-    setGridPosts(posts.slice(63, 69))
+    setGridPosts(posts.slice(64, 70))
   }, [posts])
 
   return (
