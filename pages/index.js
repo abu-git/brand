76673bef3f83f627360564fa0339d7fb0bc5894f
Samuel Abu <abu.sammie@gmail.com>
2023-12-19 11:18:54 +0200

    blaze 5th sotd

--- pages/index.js
+++ pages/index.js
@@ -64,7 +64,7 @@ export default function Home({posts, ogImageUrl }) {
           {/*<LandingHeader post={posts.data[2].attributes} />{/* -----------> Header post is chosen directly using its id */}
           <AudioPlayer />
           
-          <LandingHeader post={posts[26]} />
+          <LandingHeader post={posts[65]} />
           <LandingGrid posts={gridPosts} />
           <Playlists />
           <Footer />
