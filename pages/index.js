515132aafc17dc43908d05200972c214ed5a8d1f
Samuel Abu <abu.sammie@gmail.com>
2023-12-26 12:07:07 +0200

    mxpsom added and timi kei sotd

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home({posts, ogImageUrl }) {
   const [gridPosts, setGridPosts] = useState([])
 
   useEffect(() => {
-    setGridPosts(posts.slice(66, 72))
+    setGridPosts(posts.slice(67, 73))
   }, [posts])
 
   return (
@@ -64,7 +64,7 @@ export default function Home({posts, ogImageUrl }) {
           {/*<LandingHeader post={posts.data[2].attributes} />{/* -----------> Header post is chosen directly using its id */}
           <AudioPlayer />
           
-          <LandingHeader post={posts[47]} />
+          <LandingHeader post={posts[66]} />
           <LandingGrid posts={gridPosts} />
           <Playlists />
           <Footer />
