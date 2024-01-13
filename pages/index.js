3725ab538ea86abd44e4ab6e5a96b7de761b44aa
Samuel Abu <abu.sammie@gmail.com>
2024-01-13 18:11:52 +0200

    alpha ojini added

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home({posts, ogImageUrl }) {
   const [gridPosts, setGridPosts] = useState([])
 
   useEffect(() => {
-    setGridPosts(posts.slice(68, 74))
+    setGridPosts(posts.slice(69, 75))
   }, [posts])
 
   return (
