9b136dda4dfd47d32eab9b7c2f4a70055e178c2c
Samuel Abu <abu.sammie@gmail.com>
2024-01-21 11:02:37 +0200

    sonews gawst carrots added

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home({posts, ogImageUrl }) {
   const [gridPosts, setGridPosts] = useState([])
 
   useEffect(() => {
-    setGridPosts(posts.slice(70, 76))
+    setGridPosts(posts.slice(71, 77))
   }, [posts])
 
   return (
