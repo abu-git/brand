81869341d9aa895a753d87f4f0f8650b05f5b6ca
Samuel Abu <abu.sammie@gmail.com>
2024-03-01 13:40:42 +0200

    luda van stunna hustler prayer added

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home({ posts, ogImageUrl }) {
   const [gridPosts, setGridPosts] = useState([])
 
   useEffect(() => {
-    setGridPosts(posts.slice(72, 78))
+    setGridPosts(posts.slice(73, 79))
   }, [posts])
 
   return (
