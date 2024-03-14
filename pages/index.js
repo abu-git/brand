e2dc737dc9eee1a9cfbfcd824d7fece5557e076f
Samuel Abu <abu.sammie@gmail.com>
2024-03-14 23:52:14 +0200

    lia butler added

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home({ posts, ogImageUrl }) {
   const [gridPosts, setGridPosts] = useState([])
 
   useEffect(() => {
-    setGridPosts(posts.slice(75, 81))
+    setGridPosts(posts.slice(76, 82))
   }, [posts])
 
   return (
