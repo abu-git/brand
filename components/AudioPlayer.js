518ec832e17cd8d88fbc85ec4c06bd6b1dec14c2
Samuel Abu <abu.sammie@gmail.com>
2024-03-29 13:07:43 +0200

    ampersand fix

--- components/AudioPlayer.js
+++ components/AudioPlayer.js
@@ -79,7 +79,7 @@ const AudioPlayer = () => {
                 initial={{ y:-30 ,opacity: 0}}
                 whileInView={{ y: 0, opacity: 1}}
                 transition={{ delay: 0.4 }}
-                className='text-md md:text-lg font-semibold text-center pb-2'>Song of the Day - <span className="font-bold text-amber-600">Isiqengqe</span> by Arsenic Beats & Supah Mpondo</motion.h3>
+                className='text-md md:text-lg font-semibold text-center pb-2'>Song of the Day - <span className="font-bold text-amber-600">Isiqengqe</span> by Arsenic Beats &amp; Supah Mpondo</motion.h3>
             <div className='flex justify-center ml-6 md:ml-48 lg:ml-80'>
                 <div className={styles.audioPlayer}>
                     <audio ref={audioPlayer} src="/audio/sotd-mpondo.mp3" type="audio/mpeg" preload="auto"></audio>
