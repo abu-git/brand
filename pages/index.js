497137892fa16d70783d74a13dcea0c786078e09
Samuel Abu <abu.sammie@gmail.com>
2024-04-10 11:58:53 +0200

    sotd phlow cheque

--- pages/index.js
+++ pages/index.js
@@ -63,7 +63,7 @@ export default function Home({ posts, ogImageUrl }) {
           {/*<LandingHeader post={posts.data[2].attributes} />{/* -----------> Header post is chosen directly using its id */}
           <AudioPlayer />
           
-          <LandingHeader post={posts[56]} />
+          <LandingHeader post={posts[54]} />
           <LandingGrid posts={gridPosts} />
           <Playlists />
           <Footer />
