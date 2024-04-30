03bf3b49f356668ebe080941fe84d922366d571c
Samuel Abu <abu.sammie@gmail.com>
2024-04-30 20:28:22 +0200

    landing headers edit

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home({ posts, ogImageUrl }) {
   const [gridPosts, setGridPosts] = useState([])
 
   useEffect(() => {
-    setGridPosts(posts.slice(77, 83))
+    setGridPosts(posts.slice(78, 84))
   }, [posts])
 
   return (
