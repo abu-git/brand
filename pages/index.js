2d029cd1740925211814bf8776e0751cb418592d
Samuel Abu <abu.sammie@gmail.com>
2024-05-03 18:34:31 +0200

    sotd - odumodu

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home({ posts, ogImageUrl }) {
   const [gridPosts, setGridPosts] = useState([])
 
   useEffect(() => {
-    setGridPosts(posts.slice(78, 84))
+    setGridPosts(posts.slice(79, 85))
   }, [posts])
 
   return (
@@ -63,7 +63,7 @@ export default function Home({ posts, ogImageUrl }) {
           {/*<LandingHeader post={posts.data[2].attributes} />{/* -----------> Header post is chosen directly using its id */}
           <AudioPlayer />
           
-          <LandingHeader post={posts[84]} />
+          <LandingHeader post={posts[21]} />
           <LandingGrid posts={gridPosts} />
           <Playlists />
           <Footer />
