d3c49cb8b031638ec09d12c5498a8269c562a03a
Samuel Abu <abu.sammie@gmail.com>
2024-05-12 23:10:40 +0200

    landing header fix

--- pages/index.js
+++ pages/index.js
@@ -63,7 +63,7 @@ export default function Home({ posts, ogImageUrl }) {
           {/*<LandingHeader post={posts.data[2].attributes} />{/* -----------> Header post is chosen directly using its id */}
           <AudioPlayer />
           
-          <LandingHeader post={posts[17]} />
+          <LandingHeader post={posts[22]} />
           <LandingGrid posts={gridPosts} />
           <Playlists />
           <Footer />
