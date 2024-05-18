f06ed9f5303d0b7d6d9fb4bf38192376928bc2fa
Samuel Abu <abu.sammie@gmail.com>
2024-05-18 15:18:27 +0200

    sotd - suffer

--- pages/index.js
+++ pages/index.js
@@ -63,7 +63,7 @@ export default function Home({ posts, ogImageUrl }) {
           {/*<LandingHeader post={posts.data[2].attributes} />{/* -----------> Header post is chosen directly using its id */}
           <AudioPlayer />
           
-          <LandingHeader post={posts[81]} />
+          <LandingHeader post={posts[88]} />
           <LandingGrid posts={gridPosts} />
           <Playlists />
           <Footer />
