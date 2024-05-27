c89e56795efdf503971c3c53a8ef7a5310312fb9
Samuel Abu <abu.sammie@gmail.com>
2024-05-27 14:53:43 +0200

    sotd-illgod

--- components/LandingGridItem.js
+++ components/LandingGridItem.js
@@ -28,10 +28,10 @@ function LandingGridItem({post}) {
                     <span className='text-md lg:text-lg text-gray-700 dark:text-gray-100 mt-2 mb-2 text-justify px-3'>
                         {post.description}
                     </span>
-                    <button className='flex pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-400 dark:text-gray-500'>
+                    <button className='flex items-center pt-9 pb-4 pl-3 text-md lg:text-lg text-gray-400 dark:text-gray-500'>
                         <h4>READ MORE</h4>
                         <span>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="pl-2 pb-1 w-7 h-7">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="pl-2 w-7 h-7">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
                             </svg>
                         </span>
--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home({ posts, ogImageUrl }) {
   const [gridPosts, setGridPosts] = useState([])
 
   useEffect(() => {
-    setGridPosts(posts.slice(83, 89))
+    setGridPosts(posts.slice(84, 90))
   }, [posts])
 
   return (
@@ -63,7 +63,7 @@ export default function Home({ posts, ogImageUrl }) {
           {/*<LandingHeader post={posts.data[2].attributes} />{/* -----------> Header post is chosen directly using its id */}
           <AudioPlayer />
           
-          <LandingHeader post={posts[89]} />
+          <LandingHeader post={posts[28]} />
           <LandingGrid posts={gridPosts} />
           <Playlists />
           <Footer />
