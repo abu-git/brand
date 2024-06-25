a66753a71f26329bf7e9e7ad968859af03e19028
Samuel Abu <abu.sammie@gmail.com>
2024-06-25 14:00:09 +0200

    sotd paybac iboro

--- pages/index.js
+++ pages/index.js
@@ -63,7 +63,7 @@ export default function Home({ posts, ogImageUrl }) {
           {/*<LandingHeader post={posts.data[2].attributes} />{/* -----------> Header post is chosen directly using its id */}
           <AudioPlayer />
           
-          <LandingHeader post={posts[26]} />
+          <LandingHeader post={posts[40]} />
           <LandingGrid posts={gridPosts} />
           <Playlists />
           <Footer />
