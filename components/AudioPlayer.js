c39b2f0e206da33e36863643e39afa9f61a3bd3f
Samuel Abu <abu.sammie@gmail.com>
2024-07-12 12:32:55 +0200

    sotd iammhp everything will be fine

--- components/AudioPlayer.js
+++ components/AudioPlayer.js
@@ -79,10 +79,10 @@ const AudioPlayer = () => {
                 initial={{ y:-30 ,opacity: 0}}
                 whileInView={{ y: 0, opacity: 1}}
                 transition={{ delay: 0.4 }}
-                className='text-md md:text-lg font-semibold text-center pb-2'>Song of the Day - <span className="font-bold text-amber-600">Classic Sit Down</span> by Ngodwayne feat. LYTA</motion.h3>
+                className='text-md md:text-lg font-semibold text-center pb-2'>Song of the Day - <span className="font-bold text-amber-600">Everything Will Be Fine</span> by iamMHP feat. Brotha Nigel</motion.h3>
             <div className='flex justify-center ml-6 md:ml-48 lg:ml-80'>
                 <div className={styles.audioPlayer}>
-                    <audio ref={audioPlayer} src="/audio/sotd-ngodwayne.mp3" type="audio/mpeg" preload="auto"></audio>
+                    <audio ref={audioPlayer} src="/audio/sotd-iammhp.mp3" type="audio/mpeg" preload="auto"></audio>
                     {/*<button className={styles.forwardBackward} onClick={backThirty}><BsArrowLeftShort /> 30</button>*/}
                     <button onClick={togglePlayPause} className={styles.playPause}>
                         {isPlaying ? <FaPause /> : <FaPlay className={styles.play} />}
--- pages/index.js
+++ pages/index.js
@@ -63,7 +63,7 @@ export default function Home({ posts, ogImageUrl }) {
           {/*<LandingHeader post={posts.data[2].attributes} />{/* -----------> Header post is chosen directly using its id */}
           <AudioPlayer />
           
-          <LandingHeader post={posts[7]} />
+          <LandingHeader post={posts[94]} />
           <LandingGrid posts={gridPosts} />
           <Playlists />
           <Footer />
