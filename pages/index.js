7e6c7a33492cdbfae000cfb9ec6cd4e6cd775448
Samuel Abu <abu.sammie@gmail.com>
2024-07-22 12:25:07 +0200

    sotd - blaze 5th

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home({ posts, ogImageUrl }) {
   const [gridPosts, setGridPosts] = useState([])
 
   useEffect(() => {
-    setGridPosts(posts.slice(90, 96))
+    setGridPosts(posts.slice(91, 97))
   }, [posts])
 
   return (
@@ -63,7 +63,7 @@ export default function Home({ posts, ogImageUrl }) {
           {/*<LandingHeader post={posts.data[2].attributes} />{/* -----------> Header post is chosen directly using its id */}
           <AudioPlayer />
           
-          <LandingHeader post={posts[96]} />
+          <LandingHeader post={posts[90]} />
           <LandingGrid posts={gridPosts} />
           <Playlists />
           <Footer />
