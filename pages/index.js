563899a3fc54aebe678be144dd72b0884105442a
Samuel Abu <abu.sammie@gmail.com>
2024-07-29 11:15:01 +0200

    sotd phlow buzzin

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home({ posts, ogImageUrl }) {
   const [gridPosts, setGridPosts] = useState([])
 
   useEffect(() => {
-    setGridPosts(posts.slice(91, 97))
+    setGridPosts(posts.slice(92, 98))
   }, [posts])
 
   return (
@@ -63,7 +63,7 @@ export default function Home({ posts, ogImageUrl }) {
           {/*<LandingHeader post={posts.data[2].attributes} />{/* -----------> Header post is chosen directly using its id */}
           <AudioPlayer />
           
-          <LandingHeader post={posts[97]} />
+          <LandingHeader post={posts[54]} />
           <LandingGrid posts={gridPosts} />
           <Playlists />
           <Footer />
