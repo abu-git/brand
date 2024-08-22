0e317a15d23bb16f72cd1a69dbe2b280dd2c9894
Samuel Abu <abu.sammie@gmail.com>
2024-08-22 14:01:31 +0200

    home gridPosts changes

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home({ posts, ogImageUrl }) {
   const [gridPosts, setGridPosts] = useState([])
 
   useEffect(() => {
-    setGridPosts(posts.slice(96, 102))
+    setGridPosts(posts.slice(97, 103))
   }, [posts])
 
   return (
