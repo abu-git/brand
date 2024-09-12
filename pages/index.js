4d8e109f18ca1d109420562b8fa3198011793a48
Samuel Abu <abu.sammie@gmail.com>
2024-09-12 14:21:45 +0200

    sotd paybac iboro goat chop lion

--- pages/index.js
+++ pages/index.js
@@ -63,7 +63,7 @@ export default function Home({ posts, ogImageUrl }) {
           {/*<LandingHeader post={posts.data[2].attributes} />{/* -----------> Header post is chosen directly using its id */}
           <AudioPlayer />
           
-          <LandingHeader post={posts[62]} />
+          <LandingHeader post={posts[42]} />
           <LandingGrid posts={gridPosts} />
           <Playlists />
           <Footer />
