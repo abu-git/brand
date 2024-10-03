4799de44dfd6c257ea3703f68f3be64c67a088ba
Samuel Abu <abu.sammie@gmail.com>
2024-10-03 14:50:51 +0200

    odc sotd

--- pages/index.js
+++ pages/index.js
@@ -63,7 +63,7 @@ export default function Home({ posts, ogImageUrl }) {
           {/*<LandingHeader post={posts.data[2].attributes} />{/* -----------> Header post is chosen directly using its id */}
           <AudioPlayer />
           
-          <LandingHeader post={posts[17]} />
+          <LandingHeader post={posts[45]} />
           <LandingGrid posts={gridPosts} />
           <Playlists />
           <Footer />
