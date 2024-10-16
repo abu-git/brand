01a92256167557a830efdd045fed61fac889af2f
Samuel Abu <abu.sammie@gmail.com>
2024-10-16 14:42:16 +0200

    sotd teck summer zilla added

--- pages/index.js
+++ pages/index.js
@@ -63,7 +63,7 @@ export default function Home({ posts, ogImageUrl }) {
           {/*<LandingHeader post={posts.data[2].attributes} />{/* -----------> Header post is chosen directly using its id */}
           <AudioPlayer />
           
-          <LandingHeader post={posts[82]} />
+          <LandingHeader post={posts[112]} />
           <LandingGrid posts={gridPosts} />
           <Playlists />
           <Footer />
