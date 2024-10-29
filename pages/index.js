6c38ada3b89968b6f9c8ce946056acf31d8422b2
Samuel Abu <abu.sammie@gmail.com>
2024-10-29 11:57:12 +0200

    stoik added and sotd

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home({ posts, ogImageUrl }) {
   const [gridPosts, setGridPosts] = useState([])
 
   useEffect(() => {
-    setGridPosts(posts.slice(107, 113))
+    setGridPosts(posts.slice(108, 114))
   }, [posts])
 
   return (
@@ -63,7 +63,7 @@ export default function Home({ posts, ogImageUrl }) {
           {/*<LandingHeader post={posts.data[2].attributes} />{/* -----------> Header post is chosen directly using its id */}
           <AudioPlayer />
           
-          <LandingHeader post={posts[113]} />
+          <LandingHeader post={posts[114]} />
           <LandingGrid posts={gridPosts} />
           <Playlists />
           <Footer />
