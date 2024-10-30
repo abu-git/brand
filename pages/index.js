b466246f58c714f5d636f41c998aab73d22f7425
Samuel Abu <abu.sammie@gmail.com>
2024-10-30 12:39:42 +0200

    paybac added and sotd

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home({ posts, ogImageUrl }) {
   const [gridPosts, setGridPosts] = useState([])
 
   useEffect(() => {
-    setGridPosts(posts.slice(108, 114))
+    setGridPosts(posts.slice(109, 115))
   }, [posts])
 
   return (
@@ -63,7 +63,7 @@ export default function Home({ posts, ogImageUrl }) {
           {/*<LandingHeader post={posts.data[2].attributes} />{/* -----------> Header post is chosen directly using its id */}
           <AudioPlayer />
           
-          <LandingHeader post={posts[114]} />
+          <LandingHeader post={posts[115]} />
           <LandingGrid posts={gridPosts} />
           <Playlists />
           <Footer />
