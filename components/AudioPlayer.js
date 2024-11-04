a3f6921444f9f1f4f1fdabd261b7b2983ea500f0
Samuel Abu <abu.sammie@gmail.com>
2024-11-04 11:20:08 +0200

    sotd - priddy ugly

--- components/AudioPlayer.js
+++ components/AudioPlayer.js
@@ -79,10 +79,10 @@ const AudioPlayer = () => {
                 initial={{ y:-30 ,opacity: 0}}
                 whileInView={{ y: 0, opacity: 1}}
                 transition={{ delay: 0.4 }}
-                className='text-md md:text-lg font-semibold text-center pb-2'>Song of the Day - <span className="font-bold text-amber-600">The Goat From Lagos</span> by PayBac Iboro</motion.h3>
+                className='text-md md:text-lg font-semibold text-center pb-2'>Song of the Day - <span className="font-bold text-amber-600">30minutes to Soweto</span> by Priddy Ugly</motion.h3>
             <div className='flex justify-center ml-6 md:ml-48 lg:ml-80'>
                 <div className={styles.audioPlayer}>
-                    <audio ref={audioPlayer} src="/audio/sotd-paybac.mp3" type="audio/mpeg" preload="auto"></audio>
+                    <audio ref={audioPlayer} src="/audio/sotd-priddy.mp3" type="audio/mpeg" preload="auto"></audio>
                     {/*<button className={styles.forwardBackward} onClick={backThirty}><BsArrowLeftShort /> 30</button>*/}
                     <button onClick={togglePlayPause} className={styles.playPause}>
                         {isPlaying ? <FaPause /> : <FaPlay className={styles.play} />}
