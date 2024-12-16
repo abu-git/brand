5427dde6eadd55600623c24c72129f132361970e
Samuel Abu <abu.sammie@gmail.com>
2024-12-16 13:01:42 +0200

    amon raamadon added and sotd

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home({ posts, ogImageUrl }) {
   const [gridPosts, setGridPosts] = useState([])
 
   useEffect(() => {
-    setGridPosts(posts.slice(121, 127))
+    setGridPosts(posts.slice(122, 128))
   }, [posts])
 
   return (
@@ -62,7 +62,7 @@ export default function Home({ posts, ogImageUrl }) {
           {/*<LandingHeader post={posts.data[2].attributes} />{/* -----------> Header post is chosen directly using its id */}
           <AudioPlayer />
           
-          <LandingHeader post={posts[127]} />
+          <LandingHeader post={posts[128]} />
           <LandingGrid posts={gridPosts} />
           <Playlists />
           <Footer />
