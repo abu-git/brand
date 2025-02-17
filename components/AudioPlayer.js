cb8cec2ce5aa78912c4b2f87e4ddb70e97d0ad91
Samuel Abu <abu.sammie@gmail.com>
2025-02-17 15:13:33 +0200

    sotd - loatinover pounds...review added too

--- components/AudioPlayer.js
+++ components/AudioPlayer.js
@@ -80,11 +80,11 @@ const AudioPlayer = () => {
                 whileInView={{ y: 0, opacity: 1}}
                 transition={{ delay: 0.4 }}
                 className='text-md md:text-lg font-semibold text-center pb-2'>
-                    Song of the Day - <span className="font-bold text-amber-600">Give it all to you (feat. Casyvibez)</span> by Word Slay
+                    Song of the Day - <span className="font-bold text-amber-600">Church On Sundayz (ft. G-TECH, Thato Saul &amp; Tongue-Fu Senseii)</span> by Loatinover Pounds
             </motion.h3>
             <div className='flex justify-center ml-6 md:ml-48 lg:ml-80'>
                 <div className={styles.audioPlayer}>
-                    <audio ref={audioPlayer} src="/audio/sotd-wordslay.mp3" type="audio/mpeg" preload="auto"></audio>
+                    <audio ref={audioPlayer} src="/audio/sotd-loatinover.mp3" type="audio/mpeg" preload="auto"></audio>
                     {/*<button className={styles.forwardBackward} onClick={backThirty}><BsArrowLeftShort /> 30</button>*/}
                     <button onClick={togglePlayPause} className={styles.playPause}>
                         {isPlaying ? <FaPause /> : <FaPlay className={styles.play} />}
--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home({ posts, ogImageUrl }) {
   const [gridPosts, setGridPosts] = useState([])
 
   useEffect(() => {
-    setGridPosts(posts.slice(131, 137))
+    setGridPosts(posts.slice(132, 138))
   }, [posts])
 
   return (
@@ -63,7 +63,7 @@ export default function Home({ posts, ogImageUrl }) {
           {/*<LandingHeader post={posts.data[2].attributes} />{/* -----------> Header post is chosen directly using its id */}
           <AudioPlayer />
           
-          <LandingHeader post={posts[137]} />
+          <LandingHeader post={posts[138]} />
           <AndroidPromo />
           <LandingGrid posts={gridPosts} />
           <Playlists />
