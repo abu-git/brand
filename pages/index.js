1e3d4be9349183a6b95870a4381f0da1316747ae
Samuel Abu <abu.sammie@gmail.com>
2025-06-10 15:53:22 +0200

    sotd-keyywavv and album isigaba added

--- pages/index.js
+++ pages/index.js
@@ -63,7 +63,7 @@ export default function Home({ posts, ogImageUrl }) {
           {/*<LandingHeader post={posts.data[2].attributes} />{/* -----------> Header post is chosen directly using its id */}
           <AudioPlayer />
           
-          <LandingHeader post={posts[108]} />
+          <LandingHeader post={posts[144]} />
           <AndroidPromo />
           <LandingGrid posts={gridPosts} />
           <Playlists />
