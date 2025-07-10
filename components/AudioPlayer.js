b1ee8ee3c25818f823991acca2182ddbb5c533f8
Samuel Abu <abu.sammie@gmail.com>
2025-07-10 15:20:51 +0200

    sotd - blaze 5th

--- components/AudioPlayer.js
+++ components/AudioPlayer.js
@@ -80,11 +80,11 @@ const AudioPlayer = () => {
                 whileInView={{ y: 0, opacity: 1}}
                 transition={{ delay: 0.4 }}
                 className='text-md md:text-lg font-semibold text-center pb-2'>
-                    Song of the Day - <span className="font-bold text-amber-600">Let It Be (feat. Walter 1ders)</span> by BigBoyEllis
+                    Song of the Day - <span className="font-bold text-amber-600">Fly Akdmy Surfboards</span> by Blaze 5th
             </motion.h3>
             <div className='flex justify-center ml-6 md:ml-48 lg:ml-80'>
                 <div className={styles.audioPlayer}>
-                    <audio ref={audioPlayer} src="/audio/sotd-bbe.mp3" type="audio/mpeg" preload="auto"></audio>
+                    <audio ref={audioPlayer} src="/audio/sotd-blaze5th.mp3" type="audio/mpeg" preload="auto"></audio>
                     {/*<button className={styles.forwardBackward} onClick={backThirty}><BsArrowLeftShort /> 30</button>*/}
                     <button onClick={togglePlayPause} className={styles.playPause}>
                         {isPlaying ? <FaPause /> : <FaPlay className={styles.play} />}
