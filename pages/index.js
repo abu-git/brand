75eb20e73da3c90eaf0f3f0aafc23191664b1551
Samuel Abu <abu.sammie@gmail.com>
2025-07-27 15:03:37 +0200

    yekini sotd added

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home({ posts, ogImageUrl }) {
   const [gridPosts, setGridPosts] = useState([])
 
   useEffect(() => {
-    setGridPosts(posts.slice(140, 146))
+    setGridPosts(posts.slice(141, 147))
   }, [posts])
 
   return (
@@ -63,7 +63,7 @@ export default function Home({ posts, ogImageUrl }) {
           {/*<LandingHeader post={posts.data[2].attributes} />{/* -----------> Header post is chosen directly using its id */}
           <AudioPlayer />
           
-          <LandingHeader post={posts[146]} />
+          <LandingHeader post={posts[147]} />
           <AndroidPromo />
           <LandingGrid posts={gridPosts} />
           <Playlists />
