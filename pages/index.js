56b0db5420d042a94ff9e3a4fc2b67e62e930134
Samuel Abu <abu.sammie@gmail.com>
2025-09-09 14:21:26 +0200

    2baba kuku google am sotd

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home({ posts, ogImageUrl }) {
   const [gridPosts, setGridPosts] = useState([])
 
   useEffect(() => {
-    setGridPosts(posts.slice(145, 151))
+    setGridPosts(posts.slice(146, 152))
   }, [posts])
 
   return (
@@ -63,7 +63,7 @@ export default function Home({ posts, ogImageUrl }) {
           {/*<LandingHeader post={posts.data[2].attributes} />{/* -----------> Header post is chosen directly using its id */}
           <AudioPlayer />
           
-          <LandingHeader post={posts[151]} />
+          <LandingHeader post={posts[152]} />
           <AndroidPromo />
           <LandingGrid posts={gridPosts} />
           <Playlists />
