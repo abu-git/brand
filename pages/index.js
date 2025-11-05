131fb6964da7de5cb6e1bc6b5883423e1354c107
Samuel Abu <abu.sammie@gmail.com>
2025-11-05 18:54:54 +0200

    iammhp - foreigner added

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home({ posts, ogImageUrl }) {
   const [gridPosts, setGridPosts] = useState([])
 
   useEffect(() => {
-    setGridPosts(posts.slice(148, 154))
+    setGridPosts(posts.slice(147, 153))
   }, [posts])
 
   return (
@@ -63,7 +63,7 @@ export default function Home({ posts, ogImageUrl }) {
           {/*<LandingHeader post={posts.data[2].attributes} />{/* -----------> Header post is chosen directly using its id */}
           <AudioPlayer />
           
-          <LandingHeader post={posts[8]} />
+          <LandingHeader post={posts[153]} />
           <AndroidPromo />
           <LandingGrid posts={gridPosts} />
           <Playlists />
