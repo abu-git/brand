06f8e2f72c4da88688b75bfa7c82c4961e8af854
Samuel Abu <abu.sammie@gmail.com>
2025-12-19 18:11:30 +0200

    security patch implemented and ezra neethings sotd

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home({ posts, ogImageUrl }) {
   const [gridPosts, setGridPosts] = useState([])
 
   useEffect(() => {
-    setGridPosts(posts.slice(148, 154))
+    setGridPosts(posts.slice(149, 155))
   }, [posts])
 
   return (
@@ -63,7 +63,7 @@ export default function Home({ posts, ogImageUrl }) {
           {/*<LandingHeader post={posts.data[2].attributes} />{/* -----------> Header post is chosen directly using its id */}
           <AudioPlayer />
           
-          <LandingHeader post={posts[154]} />
+          <LandingHeader post={posts[155]} />
           <AndroidPromo />
           <LandingGrid posts={gridPosts} />
           <Playlists />
